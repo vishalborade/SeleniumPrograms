f1d2e10e7cb9b0f6b08ac0b9fb4815afba877c75
vishalborade <vishalborade@live.com>
2019-12-10 13:08:20 +0530

    this is twitter code

--- src/test/java/Testcases/Twitter_Login_Test.java
+++ src/test/java/Testcases/Twitter_Login_Test.java
@@ -1,16 +1,17 @@
 package Testcases;
 
-//to Login in FaceBook...
+//to Login in Twitter...
 //please check & update Id & Password
 import java.util.concurrent.TimeUnit;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import io.github.bonigarcia.wdm.WebDriverManager;
 
-public class FB_Login_Test 
+public class Twitter_Login_Test 
 {
 public static void main(String[] args) 
 {
@@ -19,23 +20,28 @@ public static void main(String[] args)
 	WebDriverManager.chromedriver().setup();
 	WebDriver dr = new ChromeDriver();
 	dr.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
-	String url = "https://www.facebook.com/"; 
+	String url = "https://twitter.com/"; 
 	dr.navigate().to(url);
 	dr.manage().window().maximize();
-	dr.findElement(By.cssSelector("input[id='email']")).sendKeys("1234567890");
+	dr.findElement(By.xpath("/html[1]/body[1]/div[1]/div[1]/div[1]/div[1]/div[2]/div[2]/div[1]/a[2]")).click();
+	dr.findElement(By.xpath("//input[@placeholder='Phone, email or username']")).sendKeys("12345678");
 	Thread.sleep(500);
-	dr.findElement(By.cssSelector("input[name='pass']")).sendKeys("123456789");
+	dr.findElement(By.xpath("//div[@class='clearfix field']//input[@placeholder='Password']")).sendKeys("1234567");
 	Thread.sleep(500);
-	dr.findElement(By.id("loginbutton")).click();
+	dr.findElement(By.xpath("//button[@class='submit EdgeButton EdgeButton--primary EdgeButtom--medium']")).click();
 	Thread.sleep(500);
+	//WebElement msg = dr.findElement(By.xpath("//span[@class='message-text']"));
+	Thread.sleep(500);
+	//System.out.println(msg);
+	
 	dr.quit();
+
 	}
 	catch(Exception e)
 	{
 		System.out.println("Exception has been handled...");
 	}
-	
+
 }
-	
 	
 }
